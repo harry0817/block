0ee5361269035f02ef491a7743f78c4acef78a25
Huang huajian <huanghj@zhongbosoft.com>
2019-05-07 21:01:56 +0800

    Daily commit 20190507

--- assets/scripts/Block.js
+++ assets/scripts/Block.js
@@ -3,21 +3,6 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
         bgSprite: {
             default: null,
             type: cc.Sprite
@@ -28,10 +13,10 @@ cc.Class({
         },
         point: {
             default: 0
-        }
+        },
+        row: -1,
+        col: -1
     },
-
-    // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
@@ -42,6 +27,10 @@ cc.Class({
     setPoint: function (point) {
         this.point = point;
         this.pointLabel.string = point;
+    },
+
+    setBgSprite: function (texture) {
+        this.bgSprite.texture = texture;
     }
 
     // update (dt) {},
--- assets/scripts/GameManager.js
+++ assets/scripts/GameManager.js
@@ -41,7 +41,7 @@ cc.Class({
         this.blockSize.y = this.blockSize.x * this.blockSizeRatio.y / this.blockSizeRatio.x;
         this.gamePanel.height = this.blockSize.y * 8;
 
-        this.generateBlock(this.randomPoint());
+        this.generateBlock();
     },
 
     start() {
@@ -64,35 +64,103 @@ cc.Class({
         switch (event.type) {
             case cc.Node.EventType.TOUCH_START:
             case cc.Node.EventType.TOUCH_MOVE:
-                if (this.currentBlockNode != null) {
-                    var col = this.convertPositionToCol(this.gamePanel.convertToNodeSpaceAR(event.getLocation()));
-                    var position = this.convertIndexToPosition(col, this.rowCount);
-                    this.currentBlockNode.setPosition(position.x, position.y);
+                if (this.newBlockNode != null) {
+                    let col = this.convertPositionToCol(this.gamePanel.convertToNodeSpaceAR(event.getLocation()));
+                    let position = this.convertIndexToPosition(this.rowCount, col);
+                    this.newBlockNode.setPosition(position.x, position.y);
                 }
 
                 break;
             case cc.Node.EventType.TOUCH_END:
             case cc.Node.EventType.TOUCH_CANCEL:
                 console.log("cancel");
-                if (this.currentBlockNode != null) {
-                    var col = this.convertPositionToCol(this.currentBlockNode.getPosition());
-                    var row = -1;
-                    for (let i = 0; i < this.rowCount; i++) {
-                        if (this.blockArr[col][i] == null) {
-                            row = i;
-                            break;
-                        }
-                    }
-                    if (row != -1) {
-                        var position = this.convertIndexToPosition(col, row);
-                        var action = cc.moveTo(this.moveDuration, position);
-                        this.currentBlockNode.runAction(action);
-                        this.currentBlockNode = null;
-                    } else {
-                        //游戏失败
-                    }
+                if (this.newBlockNode != null) {
+                    this.settleBlock(this.newBlockNode);
+                    this.newBlockNode = null;
                 }
                 break;
+        }
+    },
+
+    settleBlock: function (blockNode) {
+        let block = blockNode.getComponent('Block');
+        let col = this.convertPositionToCol(blockNode.getPosition());
+        let lastBlockThisCol = this.blockArr[this.rowCount - 1][col];
+        if (lastBlockThisCol != null && lastBlockThisCol.point != (block.point)) {
+            //游戏失败
+        } else {
+            let row = -1;
+            for (let i = 0; i < this.rowCount; i++) {
+                if (this.blockArr[i][col] == null) {
+                    row = i;
+                    break;
+                }
+            }
+            this.blockArr[row][col] = block;
+            block.row = row;
+            block.col = col;
+            let position = this.convertIndexToPosition(row, col);
+            let action = cc.moveTo(this.moveDuration, position);
+            blockNode.runAction(action);
+            //
+            this.scheduleOnce(function () {
+
+                this.combineBlock(blockNode, row, col);
+            }, this.moveDuration);
+        }
+    },
+
+    combineBlock: function (blockNode, row, col) {
+        console.log('combineBlock:' + row + ',' + col);
+        let block = blockNode.getComponent('Block');
+        let combineBlockArr = [];
+        let targetBlock = null;
+        let finalPoint = block.point;
+        if (row > 0 && this.blockArr[row - 1][col] != null) {//上
+            if (this.blockArr[row - 1][col].point == block.point) {
+                console.log('上');
+                targetBlock = this.blockArr[row - 1][col];
+                combineBlockArr.push(block);
+                finalPoint *= 2;
+            }
+        }
+        if (col > 0 && this.blockArr[row][col - 1] != null) {//左
+            if (this.blockArr[row][col - 1].point == block.point) {
+                console.log('左');
+                targetBlock = block;
+                combineBlockArr.push(this.blockArr[row][col - 1]);
+                finalPoint *= 2;
+            }
+        }
+        if (col < this.colCount - 1 && this.blockArr[row][col + 1] != null) {//右
+            if (this.blockArr[row][col + 1].point == block.point) {
+                console.log('右');
+                targetBlock = block;
+                combineBlockArr.push(this.blockArr[row][col + 1]);
+                finalPoint *= 2;
+            }
+        }
+
+        if (combineBlockArr.length > 0) {//可以合并
+            let targetPosition = this.convertIndexToPosition(targetBlock.row, targetBlock.col);
+            let action = cc.moveTo(this.moveDuration, targetPosition);
+            for (let i = 0; i < combineBlockArr.length; i++) {
+                let block = combineBlockArr[i];
+                block.node.runAction(action);
+            }
+            this.scheduleOnce(function () {
+                targetBlock.setPoint(finalPoint);
+                combineBlockArr.forEach(block => {
+                    let row = block.row;
+                    let col = block.col;
+                    this.blockArr[row][col] = null;
+                    block.node.destroy();
+                });
+                // this.combineBlock(blockNode, row, col);
+            }, this.moveDuration);
+
+        } else {
+            this.generateBlock();
         }
     },
 
@@ -100,10 +168,10 @@ cc.Class({
      * gamePanel中的position转换为列数
      */
     convertPositionToCol: function (position) {
-        var positionInPlayAreaX = position.x - this.paddingLeft;
+        let positionInPlayAreaX = position.x - this.paddingLeft;
         positionInPlayAreaX = Math.max(0, positionInPlayAreaX);
         positionInPlayAreaX = Math.min(this.gamePanel.width - this.paddingLeft - this.paddingRight, positionInPlayAreaX);
-        var col = Math.ceil(positionInPlayAreaX / this.blockSize.x) - 1;
+        let col = Math.ceil(positionInPlayAreaX / this.blockSize.x) - 1;
         col = Math.max(0, col);
         return col;
     },
@@ -111,30 +179,30 @@ cc.Class({
     /**
      * 行列数转换为gamePanel中的position
      */
-    convertIndexToPosition: function (col, row) {
-        var x = this.paddingLeft + this.blockSize.x / 2 + col * this.blockSize.x;
-        var y = -this.blockSize.y / 2 - row * this.blockSize.y;
+    convertIndexToPosition: function (row, col) {
+        let x = this.paddingLeft + this.blockSize.x / 2 + col * this.blockSize.x;
+        let y = -this.blockSize.y / 2 - row * this.blockSize.y;
         return new cc.Vec2(x, y);
     },
 
-    randomPoint: function () {
-        var exponent = Utils.randomNum(5);
-        return Math.pow(2, exponent);
-    },
-
-    generateBlock: function (point) {
-        var blockNode = cc.instantiate(this.blockPrefab);
+    generateBlock: function () {
+        let point = this.randomPoint();
+        let blockNode = cc.instantiate(this.blockPrefab);
         blockNode.width = this.blockSize.x;
         blockNode.height = this.blockSize.y;
         this.gamePanel.addChild(blockNode);
-        var position = this.convertIndexToPosition(Math.floor(this.colCount / 2), this.rowCount);
+        let position = this.convertIndexToPosition(this.rowCount, Math.floor(this.colCount / 2));
         blockNode.setPosition(position);
 
-        var blockComp = blockNode.getComponent("Block");
-        blockComp.setPoint(point);
-        this.currentBlockNode = blockNode;
+        let block = blockNode.getComponent("Block");
+        block.setPoint(point);
+        this.newBlockNode = blockNode;
     },
 
+    randomPoint: function () {
+        let exponent = Utils.randomNum(3);
+        return Math.pow(2, exponent);
+    },
 
     // update (dt) {},
 });
